312b56f7bf80c7d0b7f54ad98c0438ba6f1ab2ea
ankahara <hecatombe100@gmail.com>
2024-07-04 21:19:00 -0600

    crear enrutamiento

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-
+const baseUrl = "http://localhost:3000"
 const fetchApi = async (url: string) => {
   return await fetch(`${url}`).then((res) => res.json())
 }
@@ -10,13 +10,16 @@ function App() {
     init()
   }, [])
   const init = async () => {
-    const data = await fetchApi('http://localhost:3000')
+    const data = await fetchApi(baseUrl)
     setState(JSON.stringify(data))
   }
   return (
     <>
-      <h1 >Hello, World !</h1>
-      <p >{state}</p>
+    <header></header>
+    <main className="mt-10 grid place-items-center">
+      <h1 >Call for papers App</h1>
+      
+    </main>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from '@/App'
 import './index.css'
 import './pico.css'
+import LoginPage from '@/pages/LoginPage'
+import RecoverPage from '@/pages/RecoverPage'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/profile',
+        element: <div>profile</div>
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/recover',
+    element: <RecoverPage />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
